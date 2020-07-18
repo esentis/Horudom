6f2f7ef126b7a9dc3951a8ace6ece9103e4a1a97
esentis <esentakos@yahoo.gr>
2020-07-18 12:10:44 +0300

    Added Controllers
    Added  Movie Controller, not yet finished.

---
+++ Controller/MovieController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Horudom.Data;
+using Horudom.Dto;
+using Horudom.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Horudom.Controller
+{
+    [Route("api/movie")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        HorudomContext ctx;
+        public MovieController(HorudomContext Ctx)
+        {
+            ctx = Ctx;
+        }
+        [HttpGet("")]
+        public async Task<ActionResult<List<MovieDto>>> GetMovies()
+        {
+            var movies = await ctx.Movies.ToListAsync();
+
+            var result = movies.Select(x => x.ToDto()).ToList();
+            return Ok(result);
+        }
+
+    }
+}
