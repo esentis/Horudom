f1086d7d259a9193d69f7bab373d9edec4fceb5d
esentis <esentakos@yahoo.gr>
2020-08-10 11:04:56 +0300

    Creates custom log messages

--- src/Horudom.Web.Api/Helpers/HorudomLogTemplates.cs
+++ src/Horudom.Web.Api/Helpers/HorudomLogTemplates.cs
@@ -3,5 +3,18 @@ namespace Esentis.Horudom.Web.Api.Helpers
 	public static class HorudomLogTemplates
 	{
 		public const string CreatedEntity = "Created {Entity} {@Value}";
+
+		public const string FoundEntity = "Found {Entity} {@Value}";
+
+		public const string FoundEntities = "List of {Entity}s : {@Value}";
+
+		public const string Conflict = "{Entity} with {id} has assignments";
+
+		public const string Deleted = "{Entity} with {id} has been deleted";
+
+		public const string IsNull = "{Entity} is null";
+
+		public const string Updated = "{Entity} has been updated to {@NewEntity}";
+
 	}
 }
