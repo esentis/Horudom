7b2fa795168b3740b8967cc909aafb9646ad8507
esentis <esentakos@yahoo.gr>
2020-08-17 01:57:00 +0300

    Adds Screenshot Controller class

---
+++ src/Horudom.Web.Api/Controller/ScreenshotController.cs
@@ -0,0 +1,79 @@
+namespace Esentis.Horudom.Web.Api.Controller
+{
+	using System;
+	using System.Linq;
+	using System.Threading.Tasks;
+
+	using Esentis.Horudom.Web.Api.Helpers;
+	using Esentis.Horudom.Web.Models.Dto;
+
+	using global::Horudom.Data;
+	using global::Horudom.Dto;
+	using global::Horudom.Helpers;
+	using global::Horudom.Models;
+
+	using Kritikos.StructuredLogging.Templates;
+
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.Extensions.Logging;
+
+	[Route("api/screenshot")]
+	[ApiController]
+	public class ScreenshotController : BaseController<ScreenshotController>
+	{
+		public ScreenshotController(HorudomContext ctx, ILogger<ScreenshotController> logger)
+			: base(ctx, logger)
+		{
+		}
+
+		[HttpPost("")]
+		public async Task<ActionResult<ScreenshotDto>> AddScreenshot(AddScreenshotDto addScreenshotDto)
+		{
+			var movie = await Context.Movies.Where(x => x.Id == addScreenshotDto.MovieId).FirstOrDefaultAsync();
+			if (movie == null)
+			{
+				Logger.LogWarning(AspNetCoreLogTemplates.EntityNotFound, nameof(Movie), addScreenshotDto.MovieId);
+				return NotFound($"No {nameof(Movie)} with Id {addScreenshotDto.MovieId} found in database");
+			}
+
+			var screenshot = addScreenshotDto.FromDto(movie);
+			Context.Screenshots.Add(screenshot);
+			await Context.SaveChangesAsync();
+			Logger.LogInformation(HorudomLogTemplates.CreatedEntity, nameof(Screenshot), screenshot);
+			return CreatedAtAction(nameof(GetScreenshot), new { id = screenshot.Id }, screenshot.ToDto());
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<ScreenshotDto>> GetScreenshot(long id)
+		{
+			var screenshot = await Context.Screenshots.Where(x => x.Id == id).SingleOrDefaultAsync();
+
+			if (screenshot == null)
+			{
+				Logger.LogWarning(AspNetCoreLogTemplates.EntityNotFound, nameof(Screenshot), id);
+				return NotFound($"No {nameof(Screenshot)} with Id {id} found in database");
+			}
+
+			var screenshotDto = screenshot.ToDto();
+			Logger.LogInformation(HorudomLogTemplates.RequestEntity, nameof(Screenshot), id);
+			return Ok(screenshotDto);
+		}
+
+		[HttpPut("{id}")]
+		public async Task<ActionResult<ScreenshotDto>> UpdateScreenshot(int id, Uri url)
+		{
+			var screenshot = Context.Screenshots.Where(x => x.Id == id).SingleOrDefault();
+			if (screenshot == null)
+			{
+				Logger.LogWarning(AspNetCoreLogTemplates.EntityNotFound, nameof(Screenshot), id);
+				return NotFound($"No {nameof(Screenshot)} with Id {id} found in database");
+			}
+
+			screenshot.Url = url;
+			await Context.SaveChangesAsync();
+			Logger.LogInformation(HorudomLogTemplates.Updated, nameof(Screenshot), screenshot);
+			return Ok(screenshot.ToDto());
+		}
+	}
+}
