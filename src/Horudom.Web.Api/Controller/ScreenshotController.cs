fc444cf1dda7ebf99bedc042a4d1ad7d3f3c23ed
esentis <esentakos@yahoo.gr>
2020-08-17 18:57:16 +0300

    Adds download/upload
    Download and Upload functionality added

--- src/Horudom.Web.Api/Controller/ScreenshotController.cs
+++ src/Horudom.Web.Api/Controller/ScreenshotController.cs
@@ -1,7 +1,10 @@
 namespace Esentis.Horudom.Web.Api.Controller
 {
 	using System;
+	using System.IO;
 	using System.Linq;
+	using System.Reflection.Metadata;
+	using System.Security.Cryptography.X509Certificates;
 	using System.Threading.Tasks;
 
 	using Esentis.Horudom.Web.Api.Helpers;
@@ -14,17 +17,26 @@ namespace Esentis.Horudom.Web.Api.Controller
 
 	using Kritikos.StructuredLogging.Templates;
 
+	using Microsoft.AspNetCore.Http;
 	using Microsoft.AspNetCore.Mvc;
 	using Microsoft.EntityFrameworkCore;
+	using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 	using Microsoft.Extensions.Logging;
 
 	[Route("api/screenshot")]
 	[ApiController]
 	public class ScreenshotController : BaseController<ScreenshotController>
 	{
+
+		private static readonly DirectoryInfo Screenshots = new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Horudom"));
+
 		public ScreenshotController(HorudomContext ctx, ILogger<ScreenshotController> logger)
 			: base(ctx, logger)
 		{
+			if (!Screenshots.Exists)
+			{
+				Screenshots.Create();
+			}
 		}
 
 		[HttpPost("")]
@@ -42,11 +54,58 @@ namespace Esentis.Horudom.Web.Api.Controller
 				return BadRequest("Invalid Uri specified");
 			}
 
-			var screenshot = new Screenshot { Movie = movie, Url = uri };
+			var screenshot = new Screenshot { Movie = movie, FilePath = string.Empty };
 			Context.Screenshots.Add(screenshot);
 			await Context.SaveChangesAsync();
 			Logger.LogInformation(HorudomLogTemplates.CreatedEntity, nameof(Screenshot), screenshot);
 			return CreatedAtAction(nameof(GetScreenshot), new { id = screenshot.Id }, screenshot.ToDto());
+		}
+
+		[HttpPost("{movieId}/upload/")]
+		public async Task<ActionResult<ScreenshotDto>> UploadScreenshot(int movieId, IFormFile file)
+		{
+			if (file.Length == 0)
+			{
+				return BadRequest("No screenshot provided");
+			}
+
+			var movie = await Context.Movies.Where(x => x.Id == movieId).FirstOrDefaultAsync();
+			if (movie == null)
+			{
+				Logger.LogWarning(AspNetCoreLogTemplates.EntityNotFound, nameof(Movie), movieId);
+				return NotFound($"No {nameof(Movie)} with Id {movieId} found in database");
+			}
+
+			var name = Guid.NewGuid().ToString("D");
+			var path = Path.Combine(Screenshots.FullName, $"{name}.{Path.GetExtension(file.FileName)}");
+			var screenshot = new Screenshot { FilePath = path, Movie = movie};
+			using (var stream = System.IO.File.Create(path))
+			{
+				await file.CopyToAsync(stream);
+			}
+
+			Context.Screenshots.Add(screenshot);
+			await Context.SaveChangesAsync();
+			return Ok(screenshot.ToDto());
+
+		}
+
+		[HttpGet("{id}/download")]
+		public async Task<ActionResult> DownloadScreenshot(long id)
+		{
+			var screenshot = await Context.Screenshots.Where(x => x.Id == id).SingleOrDefaultAsync();
+			if (screenshot == null)
+			{
+				return NotFound($"No {nameof(Screenshot)} with Id {id} found in database");
+			}
+
+			var file = new FileInfo(screenshot.FilePath);
+			if (!file.Exists)
+			{
+				return NotFound("File has been deleted");
+			}
+
+			return File(System.IO.File.ReadAllBytes(file.FullName), "application/octet-stream", fileDownloadName: file.Name);
 		}
 
 		[HttpGet("{id}")]
@@ -75,7 +134,7 @@ namespace Esentis.Horudom.Web.Api.Controller
 				return NotFound($"No {nameof(Screenshot)} with Id {id} found in database");
 			}
 
-			screenshot.Url = url;
+			//screenshot.Url = url;
 			await Context.SaveChangesAsync();
 			Logger.LogInformation(HorudomLogTemplates.Updated, nameof(Screenshot), screenshot);
 			return Ok(screenshot.ToDto());
