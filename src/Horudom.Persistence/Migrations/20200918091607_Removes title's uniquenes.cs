c9f0e267c2825df6995151653579cb2718fd3d77
esentis <esentakos@yahoo.gr>
2020-09-18 12:17:44 +0300

    Removes title's uniqueness

---
+++ src/Horudom.Persistence/Migrations/20200918091607_Removes title's uniquenes.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Esentis.Horudom.Persistence.Migrations
+{
+    public partial class Removestitlesuniquenes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Movies_Title",
+                table: "Movies");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_Title",
+                table: "Movies",
+                column: "Title",
+                unique: true);
+        }
+    }
+}
